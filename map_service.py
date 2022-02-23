fdc210a5ff910232f208b8216f6d34969e1894aa
lubranoa <lubranoa@oregonstate.edu>
2022-02-23 12:03:59 -0600

    Add poi search and modify .gitignore
    Add basic point of interest search using the SerpAPI's Google Maps
    engine accessed via API key and GoogleSearch method of serpapi Python
    package
    
    SerpAPI key hidden in .env file and accessed using os and dotenv Python
    packages
    
    Add .env file to gitignore

--- map_service.py
+++ map_service.py
@@ -5,3 +5,24 @@
 # Description:
 #
 # -----------------------------------------------------------------------------
+
+import os
+from dotenv import load_dotenv
+from serpapi import GoogleSearch
+
+load_dotenv()
+serp_key = os.getenv('SERP_KEY')
+GoogleSearch.SERP_API_KEY = serp_key
+
+search = GoogleSearch({
+    "engine": "google_maps",
+    "type": "search",
+    "q": "houston texans stadium",
+    "google-domain": "google.com",
+    "hl": "en"
+  })
+result = search.get_dict()
+
+poi_name = result["place_results"]["title"]
+
+print(poi_name)
